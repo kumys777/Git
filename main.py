6f01784fbb46edc2b0d0e4ffbd9f47b8018b26e7
kumys777 <79105900+kumys777@users.noreply.github.com>
2022-02-20 11:52:39 +0400

    Add files via upload
    кофе

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-import pygame
-from random import randint
+import sqlite3
+import sys
+from PyQt5 import uic, QtCore
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QTableWidget, QPushButton
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI1.ui', self)
+        self.connection = sqlite3.connect("cofe_db.sqlite")
+        self.select_data()
+
+    def select_data(self):
+        query = f"SELECT * FROM cofe WHERE id "
+        cur = self.connection.cursor()
+        res = cur.execute(query).fetchall()
+        print(res)
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setRowCount(0)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        # При закрытии формы закроем и наше соединение
+        # с базой данных
+        self.connection.close()
+
 
 if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode((800, 400))
-    screen.fill(pygame.Color('blue'))
-    pygame.display.flip()
-    running = True
-    st = 0
-    st2 = 1
-    clock = pygame.time.Clock()
-    fps = 30
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = event.pos
-                st = 0
-                st2 = 0
-                a1 = randint(0, 255)
-                a2 = randint(0, 255)
-                a3 = randint(0, 255)
-        while st2 != 1:
-            pygame.draw.circle(screen, (a1, a2, a3), pos, st)
-            pygame.display.flip()
-            st += 10
-            clock.tick(fps)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                    st2 = 1
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pygame.display.flip()
-                    pos = event.pos
-                    st = 0
-                    screen.fill(pygame.Color('blue'))
-                    a1 = randint(0, 255)
-                    a2 = randint(0, 255)
-                    a3 = randint(0, 255)
-    pygame.quit()
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
